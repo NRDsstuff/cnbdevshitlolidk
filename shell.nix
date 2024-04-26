c6166fd39421131560fdbde0756d233426741ecd
nrdsstuff <nrdfoss@gmail.com>
2024-04-26 16:32:58 +0200

    lazyness 1000

--- shell.nix
+++ shell.nix
@@ -1,6 +1,7 @@
 let
   nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
   pkgs = import nixpkgs { config = { allowUnfree = true; }; overlays = []; };
+  authtoken = builtins.readFile ./.env;
 in
 
 pkgs.mkShellNoCC{
@@ -12,13 +13,13 @@ pkgs.mkShellNoCC{
     if [ -d "./site" ]; then
       cd site
       ${pkgs.nodejs_20}/bin/npm run dev
-      ngrok config add-authtoken
+      ngrok config add-authtoken ${authtoken}
       ngrok http 5173
     else
       ${pkgs.git}/bin/git clone https://github.com/raluvy95/raluvy95.github.io
       cd site
       ${pkgs.nodejs_20}/bin/npm run dev
-      ngrok config add-authtoken
+      ngrok config add-authtoken ${authtoken}
       ngrok http 5173
     fi
   '';
