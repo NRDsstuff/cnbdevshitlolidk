9731eba1ba5e52149bbb48afc1db6ef7f3bf0af4
nrdsstuff <nrdfoss@gmail.com>
2024-04-26 16:27:06 +0200

    balls itch

---
+++ shell.nix
@@ -0,0 +1,25 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
+  pkgs = import nixpkgs { config = { allowUnfree = true; }; overlays = []; };
+in
+
+pkgs.mkShellNoCC{
+  packages = with pkgs; [
+    ngrok
+  ];
+
+  shellHook = ''
+    if [ -d "./site" ]; then
+      cd site
+      ${pkgs.nodejs_20}/bin/npm run dev
+      ngrok config add-authtoken
+      ngrok http 5173
+    else
+      ${pkgs.git}/bin/git clone https://github.com/raluvy95/raluvy95.github.io
+      cd site
+      ${pkgs.nodejs_20}/bin/npm run dev
+      ngrok config add-authtoken
+      ngrok http 5173
+    fi
+  '';
+}
